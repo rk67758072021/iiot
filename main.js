a5c507d9e4b4761e47172e709a412711cc6a1500
rk67758072021 <rk6775807@gmail.com>
2023-04-13 03:58:53 +0530

    updated main_v02

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ function showPosition(position) {
 
 }
 
-function sendMail(lat, long) {
+function sendMail() {
   var link = "mailto:me20b2011@iiitdm.ac.in"
            + "?cc=myCCaddress@example.com"
            + "&subject=" + encodeURIComponent("This is my subject")
